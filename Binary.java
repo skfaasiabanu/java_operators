1a779cf922e97c8852b1f8a42d9ac2c5895612ae
skfaasiabanu <skfaasiabanu178@gmail.com>
2025-02-09 20:52:41 +0530

    binary operators

---
+++ Binary.java
@@ -0,0 +1,12 @@
+public class Binary{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=5;
+        System.out.println("Add="+(a+b));
+        System.out.println("Sub="+(a-b));
+        System.out.println("Mul="+(a*b));
+        System.out.println("Div="+(a/b));
+        System.out.println("remainer="+(a%b));
+    }
+}
