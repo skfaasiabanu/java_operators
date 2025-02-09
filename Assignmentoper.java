2b8282c17b3a1047d4e844aa0b05c61b776b4cd2
skfaasiabanu <skfaasiabanu178@gmail.com>
2025-02-09 20:24:18 +0530

    assignment operator program

---
+++ Assignmentoper.java
@@ -0,0 +1,20 @@
+public class Assignmentoper{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=5;
+        a+=20;
+        b-=5;
+        int c=23;
+        c*=2;
+        int d=56;
+        d/=3;
+        int e=100;
+        e%=2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
