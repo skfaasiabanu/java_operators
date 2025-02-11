92a47a0ac92a0c31ef1a68d3f6ea176afa6ed7e6
skfaasiabanu <skfaasiabanu178@gmail.com>
2025-02-11 20:59:28 +0530

    update

---
+++ StringToInteger.java
@@ -0,0 +1,15 @@
+public class StringToInteger
+{
+    public static void main(String args[])
+    {
+        String a="10";
+        String b="15";
+        int a1=Integer.parseInt(a);
+        int b1=Integer.parseInt(b);
+        System.out.println(a1+b1);
+        System.out.println(a1-b1);
+        System.out.println(a1*b1);
+        System.out.println(a1/b1);
+        System.out.println(a1%b1);
+    }
+}
